951a3509a76c225b9751b6ef89427a939d01b072
JeongHyun_Ku <ku2574@naver.com>
2022-01-01 04:16:27 +0900

    second commit

--- Finding_Best_k.py
+++ Finding_Best_k.py
@@ -2,9 +2,10 @@ import pyupbit
 import numpy as np
 
 
-
+coin = input("K값을 구하고 싶은 코인의 티커(ticker)를 입력하시오: ")
+coin = "KRW-" + coin
 def get_ror(k=0.5):
-    df = pyupbit.get_ohlcv("KRW-BTC",count=7)
+    df = pyupbit.get_ohlcv(coin,count=7)
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
